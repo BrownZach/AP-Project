f90c409d8de5316cb1d814d81463190dc6139f15
Zach <zachary.brown@vtsd.com>
2023-01-19 12:28:41 -0500

    Started

--- Graph.java
+++ Graph.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 import java.awt.*;
 import java.applet.*;
-public class Graphing extends Applet{
-    public void paint(Graphics g){
+public class Graph extends Applet{
+    public void paint(Graphics g) {
+//        Graph.lines;
+//        Graph.input;
+//        Graph.output;
+//    }
+//    public void lines(Graphics g){
         for(int v = 25; v < 1000; v += 25){
-
+            g.drawLine(v, 0, v, 1000);
+            g.drawLine(0, v, 1000, v);
         }
+        g.drawRect();
     }
 }
