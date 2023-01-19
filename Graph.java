d11f8a90e6d8d57c796e944bc45b5cf86b592417
Zach <zachary.brown@vtsd.com>
2023-01-19 12:20:38 -0500

    Started

---
+++ Graph.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.awt.*;
+import java.applet.*;
+public class Graphing extends Applet{
+    public void paint(Graphics g){
+        for(int v = 25; v < 1000; v += 25){
+
+        }
+    }
+}
